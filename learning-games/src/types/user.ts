e7eb055e0c9760ca20c62a2cc5caecedc12d67a6
UnnamedMistress <momchrysti@gmail.com>
2025-06-04 17:15:33 -0400

    feat: add age flow and home page

--- learning-games/src/types/user.ts
+++ learning-games/src/types/user.ts
@@ -7,4 +7,9 @@ export interface UserData {
 export interface UserContextType {
   user: UserData
   setUser: (user: UserData) => void
+  /**
+   * Update only the age field of the user. Games read this to tailor
+   * content difficulty and themes.
+   */
+  setAge: (age: number) => void
 }
