7ea2ba35a053f2058a28fdc46513a222c58258f5
UnnamedMistress <momchrysti@gmail.com>
2025-06-06 15:24:31 -0400

    fix type import in CommunityPage

--- learning-games/src/pages/CommunityPage.tsx
+++ learning-games/src/pages/CommunityPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Post, { PostData } from '../components/Post'
+import Post from '../components/Post'
+import type { PostData } from '../components/Post'
 
 const initialPosts: PostData[] = [
   {
