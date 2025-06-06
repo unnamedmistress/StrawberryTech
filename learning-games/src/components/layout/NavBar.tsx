eea57f6577f77982f29bef2227a56ef072727a55
UnnamedMistress <momchrysti@gmail.com>
2025-06-06 12:30:51 -0400

    Implement UX improvements

--- learning-games/src/components/layout/NavBar.tsx
+++ learning-games/src/components/layout/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import ThemeToggle from './ThemeToggle'
 
 export default function NavBar() {
   const [open, setOpen] = useState(false)
@@ -14,6 +15,7 @@ export default function NavBar() {
         />
         StrawberryTech
       </div>
+      <ThemeToggle />
       <button
         className="menu-toggle"
         aria-label="Toggle navigation"
