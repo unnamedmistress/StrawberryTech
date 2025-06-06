7a61e26b77d8aeda140046641008477b6ed39e94
UnnamedMistress <momchrysti@gmail.com>
2025-06-06 07:44:38 -0400

    feat: add mobile responsive navigation

--- learning-games/src/components/layout/NavBar.tsx
+++ learning-games/src/components/layout/NavBar.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <div className="brand">StrawberryTech</div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/games/match3">Match-3</Link></li>
-        <li><Link to="/games/quiz">Quiz</Link></li>
-        <li><Link to="/leaderboard">Leaderboard</Link></li>
+      <button
+        className="menu-toggle"
+        aria-label="Toggle navigation"
+        onClick={() => setOpen(o => !o)}
+      >
+        ☰
+      </button>
+      <ul className={open ? 'open' : undefined} onClick={() => setOpen(false)}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/games/match3">Match-3</Link>
+        </li>
+        <li>
+          <Link to="/games/quiz">Quiz</Link>
+        </li>
+        <li>
+          <Link to="/leaderboard">Leaderboard</Link>
+        </li>
       </ul>
     </nav>
   )
