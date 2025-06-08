8b8a16763126a47a9e251d4b55626b2a0ae63844
UnnamedMistress <momchrysti@gmail.com>
2025-06-07 20:08:13 -0400

    Add ComposeTweetGame page

---
+++ learning-games/src/pages/__tests__/ComposeTweetGame.test.tsx
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest'
+import { render, fireEvent } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import ComposeTweetGame from '../ComposeTweetGame'
+
+function setup() {
+  return render(
+    <MemoryRouter>
+      <ComposeTweetGame />
+    </MemoryRouter>
+  )
+}
+
+describe('ComposeTweetGame', () => {
+  it('unlocks door when prompt guessed correctly', () => {
+    const { getByLabelText, getByRole, getByText } = setup()
+    const input = getByLabelText(/input your guess/i)
+    fireEvent.change(input, { target: { value: 'Compose a tweet about reading a new book' } })
+    fireEvent.click(getByRole('button', { name: /submit your guess/i }))
+    expect(getByText(/door is unlocked/i)).toBeTruthy()
+  })
+})
