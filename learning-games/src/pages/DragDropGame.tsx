d180086c9609b74fd4b3619efeea706c55bbc79f
UnnamedMistress <momchrysti@gmail.com>
2025-06-09 07:26:53 -0400

    Add shared total points helper and refactor components

--- learning-games/src/pages/DragDropGame.tsx
+++ learning-games/src/pages/DragDropGame.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import ProgressSidebarSimple from '../components/layout/ProgressSidebarSimple'
 import GamePageLayout from '../components/layout/GamePageLayout'
 import { UserContext } from '../context/UserContext'
+import { getTotalPoints } from '../utils/user'
 import './DragDropGame.css'
 
 const tones = [
@@ -38,7 +39,7 @@ export default function DragDropGame() {
   const [userMessage, setUserMessage] = useState('')
   const [submitted, setSubmitted] = useState(false)
   const { user } = useContext(UserContext)
-  const totalPoints = Object.values(user.scores).reduce((a, b) => a + b, 0)
+  const totalPoints = getTotalPoints(user.scores)
   const badgesEarned = user.badges.length
 
   function handleDragStart(e: React.DragEvent<HTMLDivElement>, tone: Tone) {
